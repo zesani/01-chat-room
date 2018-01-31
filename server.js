862bb46e4891d09f81c3f2d5e0ad86a4b55dfc06
zesani <7sin@outlook.co.th>
2018-01-31 07:17:19 +0700

    init

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
-
 app.use('/', express.static('dist'))
 var numberUser = 0
 io.on('connection', function (socket) {
@@ -54,6 +53,6 @@ io.on('connection', function (socket) {
   })
 })
 
-http.listen(3000, function () {
-  console.log('listening on *:3000')
+http.listen(process.env.PORT, function () {
+  console.log('listening on *:', process.env.PORT)
 })
